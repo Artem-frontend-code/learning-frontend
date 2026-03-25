03ffb97c705674434283bbea8df288751d63b326
Artem-frontend-code <suprunartem88@gmail.com>
2026-03-25 23:11:51 +0300

    Начал вникать в стрелочные функции и немного переделал прошлые задания. Пока что не понимаю колбэки

--- 02-javascript/06-function/06-function.js
+++ 02-javascript/06-function/06-function.js
@@ -42,25 +42,33 @@ function isMax(a,b){
 console.log(isMax(100,1000));
 
 //Функция скидки 
+/*
 function discont(price, procent){
     return price - ((price/100) * procent);
-    
-
 }
 console.log(discont(1000, 20));
+*/
+
+//Функция скидки стрелочная
+
+let discont = (price, procent) => price - price / 100 * procent;
+console.log( discont(1000, 50) )
 
 //Функция каллорий
-
+/*
 let gender=prompt("Введите М - мужчина или Ж - женщина", "");
 let weight=+prompt("Введите свой вес", "");
 let height=+prompt("Введите свой рост");
 let age=+prompt("Введите свой возраст");
 let AMR = +prompt("1.2 - сидячий образ жизни,\n 1.375 - тренировки 1-3 раза в неделю,\n 1.55 - 3-5 раза в неделю,\n 1.9 - тренировка чаще раза в день");
-function calori(){
+
+const calori = () => {
     if (gender == "М"){
         return (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age))*AMR;
     } else{
         return AMR*(447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age))
     }
 }
-alert(calori());
+alert( calori() );
+*/
+
