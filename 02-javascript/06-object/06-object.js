30014f40e5309290119a6f13608adba558ae4d46
Artem-frontend-code <suprunartem88@gmail.com>
2026-03-30 20:52:46 +0300

    Сделал задание на объекты

--- 02-javascript/06-object/06-object.js
+++ 02-javascript/06-object/06-object.js
@@ -12,3 +12,18 @@ console.log(userStat);
 for (let key in userStat) {
     console.log( userStat[key] );
 }
+// Задание
+
+let getStatistics = function (players) {
+  let allGoal = 0 ;
+  for (let i = 0; i < players.length; i++) {
+    players[i].coefficient = players[i].goals * 2 + players[i].passes;
+    allGoal += players[i].goals;
+  }
+  for (let j = 0; j < players.length; j++) {
+    players[j].percent = Math.round((players[j].goals * 100) / allGoal)
+  }
+  return players
+};
+
+console.log(getStatistics([{"name":"Васька","goals":3,"passes":2},{"name":"Байт","goals":10,"passes":1},{"name":"Снежок","goals":2,"passes":14}]));
