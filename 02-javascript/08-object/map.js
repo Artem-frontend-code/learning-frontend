93d61a7c54c121bfc83f29fec25e900d51b7f63d
Artem-frontend-code <suprunartem88@gmail.com>
2026-05-07 12:42:52 +0300

    Пока допрохожу тонкости верстки, а пока закомичу как я изучил Set - набор уникальных значений

--- 02-javascript/08-object/map.js
+++ 02-javascript/08-object/map.js
@@ -3,3 +3,9 @@ const user = new Map();
 user.set("name", "Ivan");
 user.set("age", 25);
 console.log(user);
+
+const userSet = new Set();
+userSet.add("Artem");
+userSet.add("Arttem");
+userSet.add("Artem");
+console.log(userSet);
