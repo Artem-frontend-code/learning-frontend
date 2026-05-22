ac0f4aa6a8a7f57e48050f81a68d28087b78e849
Artem-frontend-code <suprunartem88@gmail.com>
2026-05-22 21:25:25 +0300

    Сделал тестовый переключатель тем, пока учу js взаимодействие с бразуером

---
+++ 01-htmlAndCSS/03-tests/grids/script.js
@@ -0,0 +1,17 @@
+let switchTheme = document.querySelector(".switch-theme");
+let card = document.querySelector(".card");
+switchTheme.onclick = function () {
+    if (switchTheme.classList.contains('black')) {
+        card.classList.add('card-black');
+        card.classList.remove('card');
+        switchTheme.classList.add('white');
+        switchTheme.classList.remove('black');
+    } else {
+        card.classList.add('card');
+        card.classList.remove('card-black');
+        switchTheme.classList.add('black');
+        switchTheme.classList.remove('white');
+
+    }
+
+}
