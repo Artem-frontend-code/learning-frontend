8f93930cf50b6d70fcba413235fa6c401e6cefe1
Artem-frontend-code <suprunartem88@gmail.com>
2026-06-30 13:32:59 +0300

    Решил задание еще и понял что это сортировка и я понял как она работает

--- 02-javascript/07-array/07-arrayTasksBook.js
+++ 02-javascript/07-array/07-arrayTasksBook.js
@@ -22,3 +22,31 @@ const sum = function (numbers) {
 console.log(range(10,1,-1));
 
 //отдыхаю но может быть задание загружу седня
+
+//Массив в обратном порядке
+
+function reverseArray (array) {
+    let newArray = [];
+    for (let i = 1; i  <= array.length; i++) {
+        newArray.push(array[array.length-i]);
+    }
+    return newArray;
+}
+
+console.log(reverseArray([1,2,3,4,5,6,7]));
+
+function reverseArrayInPlace (array) {
+    let left = 0;
+    let right = array.length-1;
+
+    while (left < right) {
+        let temp = array[left]
+        array[left] = array[right];
+        array[right] = temp;
+        left++;
+        right--;
+    }
+    return array;
+    }
+
+    console.log(reverseArrayInPlace([1,2,3,4,5,6,7,8]));
